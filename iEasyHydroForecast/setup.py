94f91cba90661f4e8c210654e169bdc63bbccbb0
Beatrice Marti <mabesa@users.noreply.github.com>
2023-12-08 16:32:08 +0100

    add requirements file

--- iEasyHydroForecast/setup.py
+++ iEasyHydroForecast/setup.py
@@ -4,7 +4,7 @@ setup(
     name='iEasyHydroForecast',
     version='0.1',
     description='A package for hydrological forecasting',
-    long_description='A package for hydrological forecasting in the SAPPHIRE project. The package workes with the iEasyHydro software.',
+    long_description='A package for hydrological forecasting in the SAPPHIRE project. The package is integrated with the SAPPHIRE Forecast Tools and with the iEasyHydro software suite.',
     packages=find_packages(),
     author='Beatrice Marti',
     author_email='marti@hydrosolutions.ch',
