ef93f1dd0f75fa5f0e07659f749f7df5dcfa6da4
Beatrice Marti <mabesa@users.noreply.github.com>
2024-07-09 10:43:54 +0200

    Update pull_docker_images.sh

--- bin/pull_docker_images.sh
+++ bin/pull_docker_images.sh
@@ -8,8 +8,11 @@ fi
 
 TAG=$1
 echo "Pulling with TAG=$TAG"
-#docker pull mabesa/sapphire-pythonbaseimage:$TAG
+docker pull mabesa/sapphire-pythonbaseimage:$TAG
 docker pull mabesa/sapphire-preprunoff:$TAG
 docker pull mabesa/sapphire-linreg:$TAG
 docker pull mabesa/sapphire-postprocessing:$TAG
 docker pull mabesa/sapphire-dashboard:$TAG
+
+# Build pipeline locally
+docker compose -f bin/docker-compose.yml build --no-cache
