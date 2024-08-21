74fe164220f5d96e4ddcf4bb4d2a27dbbd0117ab
Beatrice Marti <mabesa@users.noreply.github.com>
2024-08-21 10:39:05 +0200

    Update deploy_sapphire_forecast_tools.sh

--- bin/deploy_sapphire_forecast_tools.sh
+++ bin/deploy_sapphire_forecast_tools.sh
@@ -56,18 +56,23 @@
 #
 # Author: Beatrice Marti
 
-echo "  ____    _    ____  ____  _   _ ___ ____  _____                "
-echo " / ___|  / \  |  _ \|  _ \| | | |_ _|  _ \| ____|               "
-echo " \___ \ / _ \ | |_) | |_) | |_| || || |_) |  _|                 "
-echo "  ___) / ___ \|  __/|  __/|  _  || ||  _ <| |___                "
-echo " |____/_/   \_\_|   |_|   |_| |_|___|_| \_\_____|        _      "
-echo " |  ___|__  _ __ ___  ___ __ _ ___| |_  |_   _|__   ___ | |___  "
-echo " | |_ / _ \| '__/ _ \/ __/ _\` / __| __|   | |/ _ \ / _ \| / __| "
-echo " |  _| (_) | | |  __/ (_| (_| \__ \ |_    | | (_) | (_) | \__ \ "
-echo " |_|  \___/|_|  \___|\___\__,_|___/\__|   |_|\___/ \___/|_|___/ "
-echo "                                                                "
-echo "Deploying the SAPPHIRE forecast tools ..."
-echo "Date: $(date '+%Y-%m-%d %H:%M:%S %Z')"
+echo "|   ____    _    ____  ____  _   _ ___ ____  _____                "
+echo "|  / ___|  / \  |  _ \|  _ \| | | |_ _|  _ \| ____|               "
+echo "|  \___ \ / _ \ | |_) | |_) | |_| || || |_) |  _|                 "
+echo "|   ___) / ___ \|  __/|  __/|  _  || ||  _ <| |___                "
+echo "|  |____/_/   \_\_|   |_|   |_| |_|___|_| \_\_____|        _      "
+echo "|  |  ___|__  _ __ ___  ___ __ _ ___| |_  |_   _|__   ___ | |___  "
+echo "|  | |_ / _ \| '__/ _ \/ __/ _\` / __| __|   | |/ _ \ / _ \| / __| "
+echo "|  |  _| (_) | | |  __/ (_| (_| \__ \ |_    | | (_) | (_) | \__ \ "
+echo "|  |_|  \___/|_|  \___|\___\__,_|___/\__|   |_|\___/ \___/|_|___/ "
+echo "|                                                                 "
+echo "| Deploying the SAPPHIRE forecast tools ..."
+echo "| Date: $(date '+%Y-%m-%d %H:%M:%S %Z')"
+
+echo "|      "
+echo "| ------"
+echo "| Reading configuration"
+echo "| ------"
 
 keep_last_three_elements() {
     local path=$1
@@ -80,7 +85,7 @@ keep_last_three_elements() {
 
     # Remove the trailing slash
     result=${result%/}
-    echo "$result"
+    echo "| $result"
 }
 
 # If the argument is provided, write it to the environment variable
@@ -96,16 +101,18 @@ then
             # Test if the new env_file_path is different from the old one
             if [ "$ieasyhydroforecast_env_file_path" != "$env_file_path" ];
             then
-                  echo "WARNING: Updating ieasyhydroforecast_env_file_path\n   from $ieasyhydroforecast_env_file_path\n   to $container_env_file_path"
+                  echo "| WARNING: Updating ieasyhydroforecast_env_file_path"
+                  echo "|    from $ieasyhydroforecast_env_file_path"
+                  echo "|    to $container_env_file_path"
             fi
       fi
       # For use by the forecast tools (inside docker containers) we need to know
       # the env file path inside the docker containers as well.
       export ieasyhydroforecast_env_file_path=$container_env_file_path
-      echo "Local path to .env read from argument: $env_file_path"
-      echo "Container path to .env derived: $ieasyhydroforecast_env_file_path"
+      echo "| Local path to .env read from argument: $env_file_path"
+      echo "| Container path to .env derived: $ieasyhydroforecast_env_file_path"
 else
-      echo "Error: No path to .env file was passed or was found in the environment!"
+      echo "| Error: No path to .env file was passed or was found in the environment!"
       exit 1
 fi
 
@@ -114,8 +121,8 @@ ieasyhydroforecast_data_root_dir=$(dirname "$env_file_path")
 ieasyhydroforecast_data_ref_dir=$(dirname "$ieasyhydroforecast_data_root_dir")
 ieasyhydroforecast_data_root_dir=$(dirname "$ieasyhydroforecast_data_ref_dir")
 
-echo "ieasyhydroforecast_data_ref_dir: $ieasyhydroforecast_data_ref_dir"
-echo "ieasyhydroforecast_data_root_dir: $ieasyhydroforecast_data_root_dir"
+echo "| ieasyhydroforecast_data_ref_dir: $ieasyhydroforecast_data_ref_dir"
+echo "| ieasyhydroforecast_data_root_dir: $ieasyhydroforecast_data_root_dir"
 export ieasyhydroforecast_data_ref_dir
 export ieasyhydroforecast_data_root_dir
 
@@ -123,32 +130,46 @@ export ieasyhydroforecast_data_root_dir
 if [ -f "$env_file_path" ]; then
     source "$env_file_path"
 else
-    echo ".env file not found at $env_file_path!"
+    echo "| .env file not found at $env_file_path!"
     exit 1
 fi
 
 # If the env. varialbe ieasyhydroforecast_organization is not set, assume "demo"
 if [ -z "$ieasyhydroforecast_organization" ]; then
-  echo "WARNING: ieasyhydroforecast_organization is not set. Assuming 'demo'"
+  echo "| WARNING: ieasyhydroforecast_organization is not set. Assuming 'demo'"
   ieasyhydroforecast_organization="demo"
 fi
-echo "Deploying the SAPPHIRE forecast tools for organization:\n   $ieasyhydroforecast_organization"
+echo "| Deploying the SAPPHIRE forecast tools for organization:"
+echo "|    $ieasyhydroforecast_organization"
 
 
 # Clean up docker space
-echo "Removing all containers and images"
+echo "|      "
+echo "| ------"
+echo "| Removing all containers and images"
+echo "| ------"
 # Take down the frontend if it is running
 docker compose -f bin/docker-compose-dashboards.yml down
 ieasyhydroforecast_data_root_dir=$ieasyhydroforecast_data_root_dir source ./bin/utils/clean_docker.sh
 
+echo "|      "
+echo "| ------"
+echo "| Pulling images"
+echo "| ------"
 
 # Pull (deployment mode)
-echo "Pulling with TAG=$ieasyhydroforecast_backend_docker_image_tag"
+echo "| Pulling with TAG=$ieasyhydroforecast_backend_docker_image_tag"
 source ./bin/utils/pull_docker_images.sh $ieasyhydroforecast_backend_docker_image_tag
 
 # Establish SSH tunnel (if required)
+echo "| ieasyhydroforecast_ssh_to_iEH: $ieasyhydroforecast_ssh_to_iEH"
 if $ieasyhydroforecast_ssh_to_iEH; then
-  echo "Establishing SSH tunnel to SAPPHIRE server..."
+  echo "|      "
+  echo "| ------"
+  echo "| Connecting to iEasyHydro server"
+  echo "| ------"
+
+  echo "| Establishing SSH tunnel to SAPPHIRE server..."
   source $ieasyhydroforecast_data_ref_dir/bin/.ssh/open_ssh_tunnel.sh
 fi
 
@@ -179,8 +200,18 @@ cleanup() {
 # Set the trap to clean up processes on exit
 trap cleanup EXIT
 
+echo "|      "
+echo "| ------"
+echo "| Starting backend services"
+echo "| ------"
+
 # Start the Docker Compose service for the forecasting pipeline
 start_docker_compose_luigi
+
+echo "|      "
+echo "| ------"
+echo "| Starting frontend services"
+echo "| ------"
 
 # Start the Docker Compose service for the dashboards
 start_docker_compose_dashboards
@@ -193,14 +224,15 @@ wait $DOCKER_COMPOSE_LUIGI_PID
 #sleep 1800
 
 # Additional actions to be taken after Docker Compose service stops
-echo "Docker Compose service has finished running"
+echo "| Docker Compose service has finished running"
 
-echo "      "
-echo "------"
-echo "      "
-echo "You have now run the SAPPHIRE forecast tools for the first time!"
-echo "      "
-echo "Next steps (follow the docs for more detailed instructions):"
-echo "1. Check the logs of the Docker Compose service for any errors."
-echo "2. Check if the dashboards are running and displaying as expected."
-echo "3. Set up cron jobs for the dashboard services and for the daily run of the forecasting pipeline."
+echo "|       "
+echo "| ------"
+echo "|       "
+echo "| You have now run the SAPPHIRE forecast tools for the first time!"
+echo "|       "
+echo "| Next steps (follow the docs for more detailed instructions):"
+echo "| 1. Check the logs of the Docker Compose service for any errors."
+echo "| 2. Check if the dashboards are running and displaying as expected."
+echo "| 3. Set up cron jobs for the dashboard services and for the daily run of the forecasting pipeline."
+ECHO "| "
