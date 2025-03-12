28fbbb75647b4e9ecbf8f0026d9f9709ad5f0c37
Beatrice Marti <mabesa@users.noreply.github.com>
2025-03-12 13:27:06 +0100

    Update pull_docker_images.sh

--- bin/utils/pull_docker_images.sh
+++ bin/utils/pull_docker_images.sh
@@ -17,11 +17,11 @@ TAG=$1
 REPO="mabesa"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "SCRIPT_DIR=$SCRIPT_DIR"
-PROJECT_ROOT="$(cd "$SCRIPT_DIR/../" && pwd)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/../.." && pwd)"
 echo "PROJECT_ROOT=$PROJECT_ROOT"
 
 # By default, look for the public key in the Git repository
-COSIGN_PUBLIC_KEY="${COSIGN_PUBLIC_KEY:-$PROJECT_ROOT/keys/cosign.pub}"
+export COSIGN_PUBLIC_KEY="$PROJECT_ROOT/keys/cosign.pub"
 
 echo "Pulling with TAG=$TAG"
 
