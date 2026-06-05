da151e2abe982c55446937e308969a09aba266bb
Sandro Hunziker <145295334+sandrohuni@users.noreply.github.com>
2026-06-05 16:19:18 +0200

    forecast issue date gap fix from 10 - 5

--- apps/long_term_forecasting/run_forecast.py
+++ apps/long_term_forecasting/run_forecast.py
@@ -327,6 +327,7 @@ def run_single_model(
         else:
             logger.warning(f"Unknown data dependency type: {input_type}")
 
+
     logger.info(
         f"Head of temporal data after processing dependencies for model {model_name}:\n{temporal_data.head()}"
     )
